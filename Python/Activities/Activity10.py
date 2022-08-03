db60b79f5cda5f5fa80fac23a88ec427519008c4
Chand basha K <110019163+kcbasha@users.noreply.github.com>
2022-08-03 20:21:47 +0530

    Delete Python/Activities directory

--- Python/Activities/Activity10.py
+++
@@ -1,8 +0,0 @@
-# Given tuple
-number_tuple = (22, 25, 50, 60, 33)
-print ("Given list is", number_tuple)
-print ("Numbers that are divisible by 5:")
-for num in number_tuple:
-    if (num % 5 == 0):
-        print (num)
-
