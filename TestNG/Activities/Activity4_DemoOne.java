e0c8129e8c1cfcfb0d0f324486d5ddf2cfb32c94
Chand basha K <110019163+kcbasha@users.noreply.github.com>
2022-08-13 20:31:07 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity4_DemoOne.java
@@ -0,0 +1,15 @@
+package activities;
+
+import org.testng.annotations.Test;
+
+public class Activity4_DemoOne {
+    @Test
+    public void firstTestCase() {
+        System.out.println("I'm in 1 test case from demo 1 Class");
+    }
+
+    @Test
+    public void secondTestCase() {
+        System.out.println("I'm in 2 test case from demo 1 Class");
+    }
+}
---
+++ TestNG/Activities/Activity6.java
@@ -0,0 +1,37 @@
+package activities;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+public class Activity6 {
+    WebDriver driver;
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        driver.get("https://www.training-support.net/selenium/login-form");
+    }
+
+    @Test
+    @Parameters({"username", "password"})
+    public void loginTestCase(String username, String password) {
+               WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
+
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        String loginMessage = driver.findElement(By.id("action-confirmation")).getText();
+        Assert.assertEquals(loginMessage, "Welcome Back, admin");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        //Close browser
+        driver.quit();
+    }
+
+}
